67fe6b1a1fd384917df1243f432bd60f8f3534fe
koushik <koushik.ramaswamy@apitive.com>
2020-06-15 22:44:34 +0530

    cors, rooms to array

--- socket.js
+++ socket.js
@@ -2,36 +2,51 @@ let app = require("express")(),
   server = require("http").Server(app),
   io = require("socket.io")(server),
   port = process.env.PORT || 3000,
-  rooms = {};
+  rooms = [];
 
 server.listen(port);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res) => {
-    res.send('')
-  // res.sendFile(__dirname + "/index.html");
+  res.send("");
+});
+app.get("/rooms", (req, res) => {
+  res.send(rooms);
+});
+app.get("/room/:name", (req, res) => {
+  res.send(rooms.filter((r) => r.name === req.params.name));
 });
 io.on("connection", (socket) => {
-    socket.on('chat-message', msg => {
-        socket.broadcast.emit('incomingMessage', {
-            text: msg,
-            name: 'Jane Doe',
-            id: Math.random(),
-            timeStamp: new Date()
-        })
-    })
-  socket.on("addRoom", (name) => {
-    socket.join(name);
-    if (!rooms[name])
-      rooms[name] = {
-        users: {},
-      };
-    socket.on("new-user", (uname) => {
-      rooms[name].users[socket.id] = uname;
-      socket.to(name).broadcast.emit("user-joined", `${uname} joined`);
+  socket.on("chat-message", (msg) => {
+    socket.broadcast.emit("incomingMessage", {
+      text: msg,
+      name: "Jane Doe",
+      id: Math.random(),
+      timeStamp: new Date(),
     });
   });
+  socket.on("add-group", (name) => {
+    socket.join(name);
+    if (!rooms.some((r) => r.name === name))
+      rooms.push({
+        name,
+        users: {},
+      });
+    console.log(rooms);
+  });
+  socket.on("new-user", (uname) => {
+    rooms[name].users[socket.id] = uname;
+    socket.to(name).broadcast.emit("user-joined", `${uname} joined`);
+  });
   socket.on("disconnect", () => {
-    if (!Object.keys(rooms).length) return;
+    if (!rooms.length) return;
     let userRoom = getUserRoom(socket.id);
     if (userRoom)
       socket.leave(userRoom.room, () =>
@@ -48,11 +63,9 @@ io.on("connection", (socket) => {
 });
 
 const getUserRoom = (id) => {
-  for (const room in rooms) {
-    if (rooms.hasOwnProperty(room)) {
-      const users = rooms[room].users;
-      if (Object.keys(users).indexOf(id) > -1)
-        return { user: users[id], id, room };
-    }
+  for (let i = 0; i < rooms.length; i++) {
+    const users = rooms[i].users;
+    if (Object.keys(users).indexOf(id) > -1)
+      return { user: users[id], id, room };
   }
 };
